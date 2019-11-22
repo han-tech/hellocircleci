f3c139700ed305497348161ceb26160cd15ba548
Son Dinh <peaceman77@hotmail.com>
2019-11-22 22:49:30 +0700

    add test

---
+++ Lib/hellocirclecilib.test/ThingTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace hellocirclecilib.test
+{
+    public class ThingTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("42", new Thing().Get(19, 23));
+        }
+    }
+}
---
+++ Lib/hellocirclelib/Thing.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace hellocirclelib
+{
+    public class Thing
+    {
+        public string Get(int left, int right) => $"{left + right}";
+    }
+}
